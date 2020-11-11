ce4dfb8b305a64d3194d0d9c25aba07dced43310
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2020-11-10 22:48:51 -0300

    template v-1

---
+++ app/principal.php
@@ -0,0 +1,18 @@
+<?php 
+include 'template/header.php';
+include 'template/menu.php'; 
+?>
+
+<div class="content-inner">
+  <!-- Page Header-->
+  <header class="page-header">
+    <div class="container-fluid">
+      <h2 class="no-margin-bottom">.:Principal:.</h2>
+    </div>
+  </header>
+     
+
+  <!-- Client Section-->
+<?php include 'template/footer.php'; ?>
+
+         
--- app/template/sidebar.php
+++ app/template/sidebar.php
@@ -1,3 +1,19 @@
-<nav>
-    <p>sidebar</p>
+<!-- Side Navbar -->
+<nav class="side-navbar">
+    <!-- Sidebar Header-->
+    <div class="sidebar-header d-flex align-items-center">
+        <img src="##" style="width: 80%">
+    </div>
+    <!-- Sidebar Navidation Menus-->
+    <span class="heading">Menu</span>
+    <ul class="list-unstyled">
+        <li class="active"> <a href="principal.php"><i class="icon-home"></i>Principal</a></li>
+
+        <li><a href="#geral" aria-expanded="false" data-toggle="collapse"> <i class="icon-user"></i>Listas</a>
+            <ul id="geral" class="collapse list-unstyled">
+                <li class=""> <a href="##"><i class="icon-list"></i>###</a></li>
+                <li class=""> <a href="##"><i class="icon-list"></i>###</a></li>
+            </ul>
+        </li>
+    </ul>
 </nav>
