61339062494928ed71dbbb539cc05978ceb3efd3
deyvissoneduardo <deyvissoneduardo22@gmail.com>
2020-12-03 19:47:50 +0000

    update

--- cli/setup.sh
+++ cli/setup.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-echo "Setando variavél de ambiente em /etc/hosts"
-echo "127.0.0.1    desenv.php" >> /etc/hosts
+# ler arquivo de variaveis
+read_var=$(cat /etc/hosts | grep desenv.php)
 
+mountapp(){
+	echo "*******************Subindo aplicação******************"
+	cd ../
+	docker-compose	up -d
+}
 
-echo "subindo aplicação..."
-cd ../
-docker-compose up -d
+# faz a verificação se existe a varivavel
+if [ $? -eq 1 ]
+then 
+	echo "Setando variavél de ambiente em /etc/hosts"
+	echo "127.0.0.1    desenv.php" >> /etc/hosts
+	mountapp
+else 
+	mountapp
+fi
 
 echo "*****************************"
 echo "*   Ambienete Configurado  *"
